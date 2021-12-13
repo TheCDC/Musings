5839359d11a38386c4211a77fe3daabffa368784
Christopher Chen <christopher.chen1995@gmail.com>
2021-12-13 10:19:25 -0800

    Solve 13-1

---
+++ advent_of_code/2021/day13_1.py
@@ -0,0 +1,67 @@
+from typing import List, NewType, Tuple
+from functools import reduce
+
+PointType = NewType("PointType", Tuple[int, int])
+GridType = NewType(
+    "GridType",
+    Tuple[Tuple[int, ...], ...],
+)
+with open("inputs/day13.txt") as f:
+    points = []
+    instructions = []
+    for line in f:
+        if "," in line:
+            s = [int(i) for i in line.split(",")]
+            points.append((s[0], s[1]))
+        elif "fold" in line:
+            s = line.split("=")
+            instructions.append((s[0][-1], int(s[1])))
+
+
+def fold(grid: GridType, position: int, axis_along: str) -> GridType:
+    if axis_along == "x":
+        grid = tuple(zip(*grid))
+    newgrid = [(r[:]) for r in grid]
+    sub1 = grid[:position]
+    sub2 = tuple(reversed(grid[position + 1 :]))
+    assert len(sub2) == len(sub1)
+    assert len(sub2[0]) == len(sub1[0])
+    out = tuple(
+        [
+            tuple([max(col, sub2[irow][icol]) for icol, col in enumerate(row)])
+            for irow, row in enumerate(sub1)
+        ]
+    )
+    if axis_along == "x":
+        out = tuple(zip(*out))
+    return out
+
+
+def create_grid(points: List[PointType]) -> GridType:
+    xx, yy = reduce(lambda a, b: (max(a[0], b[0]), max(a[1], b[1])), points, points[0])
+    ps = set(points)
+    print(ps)
+    return tuple(
+        [
+            tuple([1 if (x, y) in ps else 0 for x in range(xx + 1)])
+            for y in range(yy + 1)
+        ]
+    )
+
+
+def count_points(grid):
+    return len([(ix, iy) for iy, y in enumerate(grid) for ix, x in enumerate(y) if x])
+
+
+def main():
+    print(max(x[0] for x in points), max(x[1] for x in points))
+    grid = create_grid(points)
+    print(count_points(grid))
+    folded = fold(grid, 655, axis_along="x")
+    print(count_points(folded))
+    # print(*folded, sep="\n")
+    # print(*grid, sep="\n")
+
+
+if __name__ == "__main__":
+    main()

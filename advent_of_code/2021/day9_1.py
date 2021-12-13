7f1a706ccf147b97e08ef84525ef39505e45be98
Christopher Chen <christopher.chen1995@gmail.com>
2021-12-12 22:50:31 -0800

    Solve 9-1

---
+++ advent_of_code/2021/day9_1.py
@@ -0,0 +1,45 @@
+from typing import List, TypeVar
+
+
+with open("inputs/day9.txt") as f:
+    lines = f.read().strip().split("\n")
+    grid = [[int(i) for i in line] for line in lines]
+
+GridType = TypeVar("GridType", bound=List[List[int]])
+
+
+def get_neigbors(grid: GridType, x: int, y: int):
+    offsets = [(-1, 0), (1, 0), (0, -1), (0, 1)]
+    neighbors = []
+    for o in offsets:
+        xx = x + o[0]
+        yy = y + o[1]
+        if xx >= 0 and yy >= 0:
+            try:
+                neighbors.append(grid[yy][xx])
+            except IndexError:
+                pass
+    return neighbors
+
+
+def is_lowpoint(grid: GridType, x: int, y: int):
+    return all(grid[y][x] < n for n in get_neigbors(grid, x, y))
+
+
+def risk_level(x: int):
+    return x + 1
+
+
+def main():
+    lowpoints = [
+        (col, icol, irow)
+        for irow, row in enumerate(grid)
+        for icol, col in enumerate(row)
+        if is_lowpoint(grid, icol, irow)
+    ]
+    print(sum(risk_level(x[0]) for x in lowpoints))
+    # print(sum(1 for x in lowpoints if x[0] == 0))
+
+
+if __name__ == "__main__":
+    main()

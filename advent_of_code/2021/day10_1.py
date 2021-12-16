d67358129f1954c989ec29d6a72940b2e177d5e7
Christopher Chen <christopher.chen1995@gmail.com>
2021-12-16 11:45:28 -0800

    Refactor

--- advent_of_code/2021/day10_1.py
+++ advent_of_code/2021/day10_1.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 with open("inputs/day10.txt") as f:
     lines = f.read().split()
 
@@ -6,26 +9,36 @@ closers = ")]}>"
 points = {")": 3, "]": 57, "}": 1197, ">": 25137}
 
 
-def main():
-    score_total = 0
-    for iline, line in enumerate(lines):
-        chars = list(reversed(line))
-        s = [chars.pop()]
-        score_line = 0
-        while chars:
-            c = chars.pop()
-            if c in openers:
-                s.append(c)
+def doline(line: str):
+    chars = list(reversed(line))
+    left = [chars.pop()]
+    right = []
+    score_line = None
+    while chars:
+        c = chars.pop()
+        if c in openers:
+            left.append(c)
+        else:
+            if closers.find(c) == openers.find(left[-1]):
+                left.pop()
             else:
-                if closers.find(c) == openers.find(s[-1]):
-                    s.pop()
-                else:
-                    score_line += points[c]
-                    print(iline, c, s[-1])
-                    break
-                    # error state
-        score_total += score_line
-    print(score_total)
+                score_line = points[c] if score_line is None else score_line
+                break
+    return (left, right)
+
+
+def solve(lines):
+    score_total = 0
+    completion_strings: List[str] = []
+    for iline, line in enumerate(lines):
+        l, r = doline(line)
+        completion_strings.append("".join(r))
+
+    return (score_total, completion_strings)
+
+
+def main():
+    print(solve(lines))
 
 
 if __name__ == "__main__":
